9f7740fc531c22de2d9a0df396612d02dafd2ecf
Abdul Rehaman Sk <134486438+AbdulRehaman-Shaik@users.noreply.github.com>
2024-11-30 11:01:26 +0530

    Update Armstrong_Number.sv

--- Armstrong_Number.sv
+++ Armstrong_Number.sv
@@ -6,29 +6,29 @@ class packet;
   constraint c1{num inside {[100:999]};}
   constraint c2{num dist{153 := 2, 370 := 3, 371 := 2, 407 :=3, [100:999]:/5};}
   
-  constraint c3{a == display();}
+  constraint c3{a == display(num);}
   
-  function int display();
+  function int display(int b);
     int temp;
     int rem;
     int sum = 0;
-    
-    temp = num;
+
+   	temp = b;
     $display("temp = %0d", temp);
     
-    while(temp>0)
+    while(b>0) 
       begin
-        rem = temp%10;
+        rem = b%10;
         sum = sum + (rem**3);
-        temp = temp/10;
+        b = b/10;
       end
   $display("sum = %0d",sum);
     
-    if(num == sum)
-      $display("It is Armstrong Number %0d",num);
+    if(sum == temp)
+      $display("It is Armstrong Number %0d",temp);
     else
-      $display("Not a Armstrong Number %0d",num);
-    
+      $display("Not a Armstrong Number %0d",temp);
+   
   endfunction
 endclass
 
@@ -37,9 +37,9 @@ module test();
   
   initial begin
     p1 = new();
-    repeat(100)
+    repeat(100) 
       begin
         p1.randomize();
       end
   end
-endmodule
+endmodule                  
